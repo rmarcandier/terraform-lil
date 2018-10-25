f95a2099260d54064c114769db7a971c9520c09b
Renato Marcandier <rmarcand@redhat.com>
2018-10-25 13:27:27 +0100

    terraform-networks-subdomains

--- resources.tf
+++ resources.tf
@@ -1,6 +1,6 @@
 resource "google_compute_network" "our_development_network" {
   name                    = "devnetwork"
-  auto_create_subnetworks = "true"
+  auto_create_subnetworks = false
 }
 
 resource "aws_vpc" "environment-example-two" {
@@ -9,5 +9,59 @@ resource "aws_vpc" "environment-example-two" {
   enable_dns_support = true
   tags {
     Name = "terraform-aws-vpc-example-two"
+  }
+}
+
+resource "aws_subnet" "subnet1" {
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 3, 1)}"
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+  availability_zone = "eu-west-2a"
+}
+
+resource "aws_subnet" "subnet2" {
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 2, 2)}"
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+  availability_zone = "eu-west-2b"
+}
+
+resource "aws_security_group" "subnetsecurity" {
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+
+  ingress {
+    cidr_blocks = [
+      "${aws_vpc.environment-example-two.cidr_block}"
+    ]
+
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+  }
+}
+
+resource "azurerm_resource_group" "azy_network" {
+  location = "West Europe"
+  name = "devresgrp"
+}
+
+resource "azurerm_virtual_network" "blue_virtual_network" {
+  address_space = ["10.0.0.0/16"]
+  location = "West Europe"
+  name = "bluevirtnetwork"
+  resource_group_name = "${azurerm_resource_group.azy_network.name}"
+  dns_servers = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    address_prefix = "10.0.1.0/24"
+    name = "subnet1"
+  }
+
+  subnet {
+    address_prefix = "10.0.2.0/24"
+    name = "subnet2"
+  }
+
+  tags {
+    environment = "blue-world-finder"
+
   }
 }
